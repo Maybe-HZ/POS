b8bf73953ceca3933ace1beec5949d737beb0790
Maybe-HZ <ahmadsaifulmutaqiazis1@gmail.com>
2024-02-22 09:07:11 +0700

    Initial commit

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,26 @@
+<html>
+
+<body>
+    <header>
+        <h1>Point of Sale System</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            {{-- <li><a href="{{ route('products.index') }}">Products</a></li>
+            <li><a href="{{ route('sales.index') }}">Sales</a></li>
+            <li><a href="{{ route('users.index') }}">Users</a></li> --}}
+        </ul>
+    </nav>
+    <section>
+        <h2>Welcome to Our Point of Sale System</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia minus in eligendi, aliquid quos molestiae
+            adipisci a. Laborum pariatur corrupti, blanditiis, nihil, eius perspiciatis dolore nulla consectetur
+            quisquam veritatis aspernatur?</p>
+    </section>
+    <footer>
+        &copy; 2023 ipulll
+    </footer>
+</body>
+
+</html>
